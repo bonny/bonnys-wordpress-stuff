41c3f7fbe1edbf05459b4823c9862c8e909dd3ab
Pär Thernström <par.thernstrom@gmail.com>
2013-12-18 11:19:56 +0100

    remove wpml metabox

--- functions.php
+++ functions.php
@@ -239,6 +239,10 @@ class EP {
 		remove_action('wp_head', 'rel_canonical');
 		add_filter("show_recent_comments_widget_style", "__return_false");
 
+		// Remove metabox for WPML
+		$wpml_dasbhboard_widget_id = "icl_dashboard_widget";
+		remove_meta_box( $wpml_dasbhboard_widget_id, "dashboard", "normal" );
+
 	}
 
 	/**
