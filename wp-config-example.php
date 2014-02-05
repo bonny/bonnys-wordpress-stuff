38333773827c3f4217bc10b750106ffb4e28fd87
Pär Thernström <par.thernstrom@gmail.com>
2014-02-05 14:07:30 +0100

    more config things

--- wp-config-example.php
+++ wp-config-example.php
@@ -1,17 +1,24 @@
 <?php
 
 // Set constants to detect if site is local dev, public dev, or prod
-define("EP_DEV_IS_PUBLIC", (strpos($_SERVER["HTTP_HOST"], "-dev.earthpeople.se") !== false));
+define("EP_DEV_IS_PUBLIC", (strpos($_SERVER["HTTP_HOST"], "-dev.earthpeople.se") !== false) || (strpos($_SERVER["HTTP_HOST"], ".urtp.pl") !== false));
 define("EP_DEV_IS_LOCAL", ($_SERVER["SERVER_ADDR"] === "127.0.0.1"));
 define('WP_LOCAL_DEV', (EP_DEV_IS_LOCAL || EP_DEV_IS_PUBLIC) ? true : false);
+
+// Determine if we are calling using wp cli
+define('EP_IS_WP_CLI', "cli" === php_sapi_name());
 
 if ( EP_DEV_IS_PUBLIC || EP_DEV_IS_LOCAL ) {
 
 	// If local private or public dev
 
 	// Always debug when on local
-	define('WP_DEBUG', true);
-	define('JETPACK_DEV_DEBUG', true);
+	if (EP_DEV_IS_LOCAL) {
+
+		define('JETPACK_DEV_DEBUG', true);
+		define('WP_DEBUG', true);
+
+	}
 
 	define('DB_NAME', '');
 	define('DB_USER', '');
