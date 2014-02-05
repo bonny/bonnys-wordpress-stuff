1709520bd26b19f2e464da8c924660ea4736f6c2
Pär Thernström <par.thernstrom@gmail.com>
2014-02-05 14:07:22 +0100

    correct paths for scripts

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -3,8 +3,8 @@
 <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri() ?>/js/jquery/jquery.min.js"><\/script>')</script>
 
 <!--[if (gte IE 6)&(lte IE 8)]>
-	<script src="<?php echo get_template_directory_uri() ?>/js/jquery-extra-selectors.js"></script>
-	<script src="<?php echo get_template_directory_uri() ?>/js/selectivizr.js"></script>
+	<script src="<?php echo get_template_directory_uri() ?>/js/jquery-extended-selectors/jquery-extra-selectors.js"></script>
+	<script src="<?php echo get_template_directory_uri() ?>/js/selectivizr/selectivizr.js"></script>
 <![endif]-->
 
 <?php wp_footer(); ?>
