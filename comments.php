f9bdafcd00282a0cf9266125fe8713cb9a003e3b
Pär Thernström <par.thernstrom@gmail.com>
2014-04-22 19:30:34 +0200

    use basic comment formatting from tweny fourteen

--- comments.php
+++ comments.php
@@ -1,48 +1,64 @@
 <?php
 /**
- * Template for displaying Comments.
+ * The template for displaying Comments
  *
- * The area of the page that contains both current comments
- * and the comment form. The actual display of comments is
- * handled by a callback to starkers_comment() which is
- * located in the functions.php file.
+ * The area of the page that contains comments and the comment form.
  *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
  */
+
+/*
+ * If the current post is protected by a password and the visitor has not yet
+ * entered the password we will return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
 ?>
-<div id="comments">
-	<?php if ( post_password_required() ) : ?>
-	<p>This post is password protected. Enter the password to view any comments</p>
-</div>
 
-	<?php
-			/* Stop the rest of comments.php from being processed,
-			 * but don't kill the script entirely -- we still have
-			 * to fully load the template.
-			 */
-			return;
-		endif;
-	?>
-
-	<?php // You can start editing here -- including this comment! ?>
+<div id="comments" class="comments-area">
 
 	<?php if ( have_comments() ) : ?>
 
-	<h2><?php comments_number(); ?></h2>
+	<h2 class="comments-title">
+		<?php
+			printf( _n( 'En kommentar på &ldquo;%2$s&rdquo;', '%1$s kommentarer på &ldquo;%2$s&rdquo;', get_comments_number(), 'twentyfourteen' ),
+				number_format_i18n( get_comments_number() ), get_the_title() );
+		?>
+	</h2>
 
-	<ol>
-		<?php wp_list_comments( array( 'callback' => 'starkers_comment' ) ); ?>
-	</ol>
+	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+	<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+		<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'twentyfourteen' ); ?></h1>
+		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Äldre kommentarer', 'twentyfourteen' ) ); ?></div>
+		<div class="nav-next"><?php next_comments_link( __( 'Nyare kommentarer &rarr;', 'twentyfourteen' ) ); ?></div>
+	</nav><!-- #comment-nav-above -->
+	<?php endif; // Check for comment navigation. ?>
 
-	<?php
-		/* If there are no comments and comments are closed, let's leave a little note, shall we?
-		 * But we don't want the note on pages or post types that do not support comments.
-		 */
-		elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-	
-	<p>Comments are closed</p>
-	
+	<ol class="comment-list">
+		<?php
+			wp_list_comments( array(
+				'style'      => 'ol',
+				'short_ping' => true,
+				'avatar_size'=> 34,
+			) );
+		?>
+	</ol><!-- .comment-list -->
+
+	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+	<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Äldre kommentarer', 'twentyfourteen' ) ); ?></div>
+		<div class="nav-next"><?php next_comments_link( __( 'Nyare kommentarer &rarr;', 'twentyfourteen' ) ); ?></div>
+	</nav><!-- #comment-nav-below -->
+	<?php endif; // Check for comment navigation. ?>
+
+	<?php if ( ! comments_open() ) : ?>
+	<p class="no-comments"><?php _e( 'Kommenteringen är avstängd.', 'twentyfourteen' ); ?></p>
 	<?php endif; ?>
+
+	<?php endif; // have_comments() ?>
 
 	<?php comment_form(); ?>
 
